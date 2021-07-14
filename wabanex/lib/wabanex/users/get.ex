50764fc4f32374705da78d0d902272d2d36e4308
joaobnd <joao.martins@ccc.ufcg.edu.br>
2021-07-13 23:28:41 -0300

    commitando aula 2

---
+++ wabanex/lib/wabanex/users/get.ex
@@ -0,0 +1,22 @@
+defmodule Wabanex.Users.Get do
+
+  alias Wabanex.{Repo, User}
+  alias Ecto.UUID
+
+  def call(id) do
+    id
+    |>UUID.cast()
+    |>handle_response()
+  end
+
+  defp handle_response(:error) do
+    {:error, "id invalido"}
+  end
+
+  defp handle_response({:ok, uuid}) do
+    case Repo.get(User, uuid) do
+      nil -> {:error, "usuario nao encontrado"}
+      user -> {:ok, user}
+    end
+  end
+end
